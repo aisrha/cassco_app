6d0172191cc730f0327a48147e28fc121f81477b
Alifah Ilyana <alifaahilyanaa@gmail.com>
2022-06-18 00:54:14 +0800

    Update formative screen
    - topic 2 and topic 3

---
+++ lib/screens/formative/formative3_screen.dart
@@ -0,0 +1,75 @@
+import 'package:casscoapp/main.dart';
+import 'package:casscoapp/model/question_model.dart';
+import 'package:casscoapp/widgets/question_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Formative3Screen extends StatefulWidget {
+  const Formative3Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Formative3Screen> createState() => _Formative3ScreenState();
+}
+
+class _Formative3ScreenState extends State<Formative3Screen> {
+  late Stream<QuerySnapshot<Map<String, dynamic>>> _quiz1;
+  @override
+  void initState() {
+    super.initState();
+    _quiz1 = FirebaseFirestore.instance.collection('topic3').snapshots();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var state = Provider.of<AppState>(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            state.emptyArrayList();
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.close),
+        ),
+        title: const Text('Formative Assessment : Topic 3'),
+        backgroundColor: Colors.orangeAccent,
+        centerTitle: true,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _quiz1,
+        builder: (context, snapshot) {
+          var state = Provider.of<AppState>(context);
+          if (snapshot.hasError) {
+            return const Text('Something went wrong!');
+          }
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return PageView(
+            physics: const NeverScrollableScrollPhysics(),
+            controller: state.controller,
+            children: snapshot.data!.docs
+                .map(
+                  (question) => QuestionWidget(
+                    question: QuestionModel(
+                      question: question['question'],
+                      ans1: question['ans1'],
+                      ans2: question['ans2'],
+                      ans3: question['ans3'],
+                      ans4: question['ans4'],
+                      correctAns: question['correctAns'],
+                      quesID: question['quesID'],
+                    ),
+                    controller: state.controller,
+                  ),
+                )
+                .toList(),
+          );
+        },
+      ),
+    );
+  }
+}
