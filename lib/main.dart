a35a386fbcf28f3da9000c99238178c04726c63f
Alifah Ilyana <alifaahilyanaa@gmail.com>
2022-06-17 11:42:23 +0800

    Add provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,59 @@
 import 'package:casscoapp/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AppState with ChangeNotifier {
+  List<String> _correctAnsList = [];
+  List<String> _userAnswerList = [];
+  int _score = 0;
+
+  final PageController controller = PageController();
+
+  get correctAnsList => _correctAnsList;
+  get userAnswerList => _userAnswerList;
+  get score => _score;
+
+  void addScore(int score) {
+    _score = _score + score;
+  }
+
+  void addCorrectAnsList(String correctAns) {
+    _correctAnsList.add(correctAns);
+  }
+
+  void addUserAnsList(String userAns) {
+    _userAnswerList.add(userAns);
+  }
+
+  void emptyArrayList() {
+    _correctAnsList.clear();
+    _userAnswerList.clear();
+    _score = 0;
+  }
+
+  void nextPage() async {
+    await controller.nextPage(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeOut,
+    );
+  }
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AppState(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
