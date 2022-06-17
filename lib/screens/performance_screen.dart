4fe0d4cbedd949fb658f8de8cba78051a1dd52fc
Alifah Ilyana <alifaahilyanaa@gmail.com>
2022-06-17 11:41:17 +0800

    Add performance screen

---
+++ lib/screens/performance_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PerformanceScreen extends StatefulWidget {
+  PerformanceScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PerformanceScreen> createState() => _PerformanceScreenState();
+}
+
+class _PerformanceScreenState extends State<PerformanceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
