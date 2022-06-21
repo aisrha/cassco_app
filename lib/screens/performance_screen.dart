f6a1f7b94056a856c5636590925cff7b90c71890
Alifah Ilyana <alifaahilyanaa@gmail.com>
2022-06-22 00:05:43 +0800

    Update performance_screen.dart
    - view all performance
    - check attempt before viewing

--- lib/screens/performance_screen.dart
+++ lib/screens/performance_screen.dart
@@ -1,3 +1,7 @@
+import 'package:casscoapp/screens/performance/formativeperformance_screen.dart';
+import 'package:casscoapp/screens/performance/summativeperformance_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PerformanceScreen extends StatefulWidget {
@@ -10,6 +14,105 @@ class PerformanceScreen extends StatefulWidget {
 class _PerformanceScreenState extends State<PerformanceScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    User? user = FirebaseAuth.instance.currentUser;
+
+    // check attempt
+    Future checkAttempt(String formativeTopic, String dbCollection) async {
+      return FirebaseFirestore.instance
+          .collection('users')
+          .doc(user!.uid)
+          .collection(dbCollection)
+          .get()
+          .then(
+        (value) {
+          var count = value.docs.length;
+
+          if (count == 0) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: const Text(
+                  'Oops! Sorry, you have not attempted this assesment.'),
+              action: SnackBarAction(
+                label: 'Dismiss',
+                onPressed: () {
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                },
+              ),
+            ));
+          } else if (count > 0) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => FormativePerformanceScreen(
+                          formativeTopic: formativeTopic,
+                          dbCollection: dbCollection,
+                        )));
+          }
+        },
+      );
+    }
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(23),
+        child: ListView(
+          children: [
+            const Text(
+              'Performance',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(height: 25),
+            SizedBox(
+              height: 220,
+              child: Image.asset("assets/trophy.png", fit: BoxFit.contain),
+            ),
+            const SizedBox(height: 25),
+            GestureDetector(
+              onTap: () {
+                checkAttempt('Topic 1', 'topic1');
+              },
+              child: const ListTile(
+                leading: Icon(Icons.filter_1),
+                title: Text('Formative : Topic 1'),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                checkAttempt('Topic 2', 'topic2');
+              },
+              child: ListTile(
+                tileColor: Colors.grey.shade100,
+                leading: const Icon(Icons.filter_2),
+                title: const Text('Formative : Topic 2'),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                checkAttempt('Topic 3', 'topic3');
+              },
+              child: const ListTile(
+                leading: Icon(Icons.filter_3),
+                title: Text('Formative : Topic 3'),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SummativePerformanceScreen()));
+              },
+              child: ListTile(
+                tileColor: Colors.grey.shade100,
+                leading: const Icon(Icons.filter_4),
+                title: const Text('Summative : All topics'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
